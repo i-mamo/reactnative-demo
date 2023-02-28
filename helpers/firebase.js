2a5b0920cb104f926cca8f8c4976e654b33a4a1e
mamo <mmr0809sk@gmail.com>
2023-03-01 03:14:38 +0900

    g

--- helpers/firebase.js
+++ helpers/firebase.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+require("dotenv").config()
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC09nr8dZdzmUit4jzxD33De89DtmjtFfg",
-  authDomain: "reactnative-demo-7f87e.firebaseapp.com",
-  projectId: "reactnative-demo-7f87e",
-  storageBucket: "reactnative-demo-7f87e.appspot.com",
-  messagingSenderId: "336292636402",
-  appId: "1:336292636402:web:5b54c27a416338df8c126b"
+  apiKey: process.env.apiKey,
+  authDomain:process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 // Initialize Firebase
